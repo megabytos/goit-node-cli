5f9847a899cdf7bd15b3df4df56d54591cea4cbf
Alex Goliansky <a_goliansky@viyar.ua>
2025-02-26 17:19:58 +0200

    HW1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# goit-node-cli
+# Node.js Development HW1 - CLI & files
+* CLI application for CRUD managing contacts in JSON file
+
---
+++ index.js
@@ -0,0 +1,46 @@
+import {program} from "commander";
+import {addContact, getContactById, listContacts, removeContact, updateContact} from "./contacts.js";
+
+
+program
+    .option("-a, --action <type>", "choose action")
+    .option("-i, --id <type>", "user id")
+    .option("-n, --name <type>", "user name")
+    .option("-e, --email <type>", "user email")
+    .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+
+async function invokeAction({action, id, name, email, phone}) {
+    switch (action) {
+        case "list":
+            console.table(await listContacts());
+            break;
+
+        case "get":
+            if (!id) throw new Error("ID required.");
+            console.log(await getContactById(id))
+            break;
+
+        case "add":
+            console.log(await addContact(name, email, phone))
+            break;
+
+        case "remove":
+            if (!id) throw new Error("ID required.");
+            console.log(await removeContact(id))
+            break;
+
+        case "update":
+            if (!id) throw new Error("ID required.");
+            console.log(await updateContact(id, name, email, phone))
+            break;
+
+        default:
+            console.warn("\x1B[31m Unknown action type!");
+    }
+}
+
+invokeAction(options).catch(error => console.error("Error:", error.message));
