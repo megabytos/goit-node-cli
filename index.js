9beb379d494cc69f2ae64e633d4eb0f9b31759d5
Alex Goliansky <a_goliansky@viyar.ua>
2025-03-01 12:54:36 +0200

    add try/catch

--- index.js
+++ index.js
@@ -14,33 +14,39 @@ program.parse();
 const options = program.opts();
 
 async function invokeAction({action, id, name, email, phone}) {
-    switch (action) {
-        case "list":
-            console.table(await listContacts());
-            break;
+    try {
+        switch (action) {
+            case "list":
+                console.table(await listContacts());
+                break;
 
-        case "get":
-            if (!id) throw new Error("ID required.");
-            console.log(await getContactById(id))
-            break;
+            case "get":
+                if (!id) throw new Error("ID required.");
+                console.log(await getContactById(id))
+                break;
 
-        case "add":
-            console.log(await addContact(name, email, phone))
-            break;
+            case "add":
+                console.log(await addContact(name, email, phone))
+                break;
 
-        case "remove":
-            if (!id) throw new Error("ID required.");
-            console.log(await removeContact(id))
-            break;
+            case "remove":
+                if (!id) throw new Error("ID required.");
+                console.log(await removeContact(id))
+                break;
 
-        case "update":
-            if (!id) throw new Error("ID required.");
-            console.log(await updateContact(id, name, email, phone))
-            break;
+            case "update":
+                if (!id) throw new Error("ID required.");
+                console.log(await updateContact(id, name, email, phone))
+                break;
 
-        default:
-            console.warn("\x1B[31m Unknown action type!");
+            default:
+                console.warn("\x1B[31m Unknown action type!");
+        }
+    } catch (error) {
+        console.error("\x1B[31mError:", error.message);
     }
 }
 
-invokeAction(options).catch(error => console.error("Error:", error.message));
+(async () => {
+    await invokeAction(options).catch(error => console.error("Error:", error.message));
+})();
